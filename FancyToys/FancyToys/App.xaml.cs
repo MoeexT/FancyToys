8120167a0adf3fbede2c547b554ee6b09b1fbc36
MoeexT <yuwancumian666@outlook.com>
2022-04-15 00:56:13 +0800

    resize window

--- FancyToys/FancyToys/App.xaml.cs
+++ FancyToys/FancyToys/App.xaml.cs
@@ -1,28 +1,16 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
+﻿using System;
+using System.Runtime.InteropServices;
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using WinRT;
 
+using Microsoft.UI.Xaml;
 using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace FancyToys
-{
+namespace FancyToys {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
@@ -37,23 +25,32 @@ namespace FancyToys
             this.InitializeComponent();
         }
 
+        private void OnSuspending(object sender, SuspendingEventArgs e) {
+            //This is never called in WInUi 3 in Desktop Preview 3
+        }
+
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
+        /// https://github.com/microsoft/WinUI-3-Demos/blob/420c48fe1613cb20b38000252369a0c556543eac/src/Build2020Demo/DemoBuildCs/DemoBuildCs/DemoBuildCs/App.xaml.cs#L41
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args) {
             m_window = new MainWindow();
+            //Get the Window's HWND
+            var windowNative = m_window.As<IWindowNative>();
+            WindowHandle = windowNative.WindowHandle;
+            m_window.Title = "FancyToys";
             m_window.Activate();
+
+            // The Window object doesn't have Width and Height properties in WInUI 3 Desktop yet.
+            // To set the Width and Height, you can use the Win32 API SetWindowPos.
+            // Note, you should apply the DPI scale factor if you are thinking of dpi instead of pixels.
+            SetWindowSize(WindowHandle, 900, 600);
         }
 
-        private Window m_window;
-        
-        
-        private void SetWindowSize(IntPtr hwnd, int width, int height)
-        {
-            var dpi = PInvoke.User32.GetDpiForWindow(hwnd);
+        private static void SetWindowSize(IntPtr hwnd, int width, int height) {
+            int dpi = PInvoke.User32.GetDpiForWindow(hwnd);
             float scalingFactor = (float)dpi / 96;
             width = (int)(width * scalingFactor);
             height = (int)(height * scalingFactor);
@@ -61,6 +58,16 @@ namespace FancyToys
             PInvoke.User32.SetWindowPos(hwnd, PInvoke.User32.SpecialWindowHandles.HWND_TOP,
                 0, 0, width, height,
                 PInvoke.User32.SetWindowPosFlags.SWP_NOMOVE);
+        }
+
+        private Window m_window;
+        public IntPtr WindowHandle { get; private set; }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative {
+            IntPtr WindowHandle { get; }
         }
     }
 }
--- FancyToys/FancyToys/Logging/Dogger.cs
+++ FancyToys/FancyToys/Logging/Dogger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
+using System.Threading.Tasks;
 
 using FancyToys.Views;
 
